8649f3c3047af59143e625926c1433cba5b71598
joung99 <joung3821@naver.com>
2017-02-06 14:18:00 +0900

    remote repository add a HelloGitHub.java

---
+++ HelloGitHub/src/com/sist/HelloGItHub.java
@@ -0,0 +1,11 @@
+package com.sist;
+
+public class HelloGItHub {
+	public static void main(String[] args){
+		System.out.println("Hello Git");
+		System.out.println("What's Git?");
+		System.out.println("It's collaboration");
+		System.out.println("It's DVCS");
+		System.out.println("Conflict");
+	}
+}
